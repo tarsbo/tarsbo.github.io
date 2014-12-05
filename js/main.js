5d33cbd1363bf08bc8bf19ddca6fe34c3237b4d9
Tara Boland <bolandtara@hotmail.com>
2014-12-05 11:11:56 -0500

    trying to fix flex slider for images

--- js/main.js
+++ js/main.js
@@ -20,3 +20,16 @@ $(".logowrap").waypoint(function(direction){
 	}
 	
 },{offset:"22px"})
+
+
+
+$(".piece").on("click",function(){
+	var id=$(this).attr("data-id");
+	$(id).attr("data-state","open");
+	
+	});
+	
+$(".close").on("click",function(){
+	$(this).parent().attr("data-state","close");
+	
+	});
